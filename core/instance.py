b7b55d38d9e5a4278d9f96bca7d48e89cd082cdc
Oneplus <oneplus.lau@gmail.com>
2012-10-07 22:20:15 +0800

    revise instance.py

--- core/instance.py
+++ core/instance.py
@@ -1,41 +1,19 @@
+
+
 class Instance(object):
-    """ """
-    def __init__(self, content, format="CONLL", labeled=True):
+    """ basic data struct for a instance
+    """
+    def __init__(self):
         """
         @param  content instance in string form
         @param  format  specify format of the content
         """
-        self.fv = None
-        self.actParseTree = None
-
-        self.forms = None
-        self.lemmas = None
-        self.cpostags = None
-        self.postags = None
-        self.feats = None
-        self.heads = None
-        self.deprels = None
-        self.relFeats = None
-        self.confidenceScore = None
-
-        if format == "CONLL":
-            lines = content.split("\n")
-            self.forms = map(lambda l: l.split("\t")[1], lines)
-            self.lemmas = map(lambda l: l.split("\t")[2], lines)
-            self.cpostags = map(lambda l: l.split("\t")[3], lines)
-            self.postags = map(lambda l: l.split("\t")[4], lines)
-            self.feats = map(lambda l: l.split("\t")[5].split("|"), lines)
-            self.heads = map(lambda l: int(l.split("\t")[6]), lines)
-            self.deprels = map(lambda l: l.split("\t")[7] if labeled else 
-                    "<no-type>", lines)
-
-        elif format == "SEG":
-            self.forms = content.split()
-
-        elif format == "POS":
-            separator = "/"
-            self.forms = map(lambda wordtag: split(separator)[0], content.split())
-            self.postag = map(lambda wordtag: split(separator)[1], content.split())
+        self.raw        = None
+        self.forms      = None
+        self.lemmas     = None
+        self.cpostags   = None
+        self.postags    = None
+        self.heads      = None
 
     def __len__(self):
         if self.forms is not None:
